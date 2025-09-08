7ca8c6d520b8971089c93e0046b3d66509d2c5ab
jyni22 <kathiajay125@Gmail.com>
2025-09-08 15:52:15 +0000

    define variables.

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,19 @@
-console.log('Thor');
+const { act } = require("react");
+
+// console.log('Thor');
+const accountId = 788912;
+let accountEmail ="kal34@gmail.com";
+var accountPassword = "022032";
+accountCity = "Junagadh";
+let accountState = "";
+
+// accountId = 2354; // not allowed
+
+accountEmail = "Atr0321@gmail.com";
+accountPassword = "900978";
+accountCity = "Rajkot";
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
