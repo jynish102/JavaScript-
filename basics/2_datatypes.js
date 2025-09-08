dfaa11973f1338bf825faad556b02bd8769b65e9
jyni22 <kathiajay125@Gmail.com>
2025-09-08 15:53:12 +0000

    define datatypes

---
+++ basics/2_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" ; // treat all js Code as newer version
+
+// alert( 20 + 56 ) // using nodejs,not broser
+
+console.log( 20 
+    + 89 ); //code redebility high
+console.log("dr. Strange");
+
+let name = "Thanos";
+let age = 56;
+let isLoggedin = false;
+
+// number => 2 to poe 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+// console.log(typeof "Thanos");
+console.log(typeof null);
+console.log(typeof undefined);
