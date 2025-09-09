201b591f1162a3d99560d0c346e02967ba63624c
jyni22 <kathiajay125@Gmail.com>
2025-09-09 21:36:52 +0530

    Add files via upload
    type Conversion also string

---
+++ basics/3conversion.js
@@ -0,0 +1,53 @@
+let number1 = "2356";
+ let number2 = 2356 ;
+ let number3 = "2356hik";
+ let number4 = null ;
+ let number5 = ""; //put as empty or declare undefined both are same 
+ let number6 = true ;
+ let number7 = "Thanos";
+//  let valueInNumber = Number(number3);
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+//  console.log(number7);
+// console.log(typeof number7)
+
+// let isRegisterIn = 1;
+// let booleanIsRegisterIn = Boolean(isRegisterIn)
+// console.log(booleanIsRegisterIn);
+
+// let number8 = 2312631;
+// let stringNumber = String(number8);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// "4561" => 4561
+// "2356hik" => Nan
+// true => 1, fasle => 0
+
+
+//-----------------------------------Operations---------------------------------------------
+
+// let value = 78;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(10 + 12);
+// console.log(22 - 12);
+// console.log(10 * 12);
+// console.log(12**4);
+// console.log(10 / 12);
+// console.log(10 % 12);
+
+// let str1 = "Thanos";
+// let str2 = " is Villain of";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("10" + 12);
+// console.log(10 + "12");
+// console.log("10" + 12 + 22);
+// console.log(10 + 12 + "10");
+// console.log(false);
+// console.log(+false);
+// console.log(+"");
