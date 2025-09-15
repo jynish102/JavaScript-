338f7bcefaa9e468c2e106ceb0bee7f1657b0b5e
jyni22 <jaykal67kal@gmail.com>
2025-09-15 11:12:00 +0530

    Update and rename 1_function.js to 1_function.js

--- 3_basics/1_function.js
+++ 3_basics/1_function.js
@@ -85,12 +85,86 @@ let item = ["Laptop",
         "Tablet",
         "Smartwatch",
         "Headphones"
-    ]
+           ]
 
 function handleArray(anyarray){
      return `${anyarray[0]} ${anyarray[1]}`
-
 }
 
 console.log(handleArray(item))
 /* direct array to function call */
+
+//-------------------------------video : 23 Arrow Function-----------------------
+
+let laptop2 = {
+    brand : "Dell",
+    model : "XPS 15",
+    price : 50000,
+    ram: "16GB",
+    storage: "512GB SSD",
+    processor: "Intel i7",
+    
+    detail :function(){
+        console.log(`Laptop brand is ${this.brand} and Model is ${this.model}`)
+
+    }
+};
+
+// laptop2.detail()
+// laptop2.brand = "hp"
+// laptop2.detail() 
+/* no any error use this here  */
+
+// function one(){
+//     let brand = "dell"
+//     // console.log(this) no error
+//     console.log(this.brand) \
+//     /* undefined in function this not work */
+// }
+// one()
+
+
+// let one = function (){
+//     let brand = "hp"
+//     console.log(this.brand)
+// }
+// one()
+
+/* same as above i.e  i have not to store function in variable that
+case */
+
+// let one = () =>{
+//     let brand = "hp"
+//     console.log(this.brand);
+// }
+// one()
+
+//explicit arrow function
+// let addTwo = (g1,g4) => {
+//     return g1 + g4
+// }
+// console.log(addTwo(3,5))
+
+/*implicit return no need {} or any return. assume that something is 
+return   if use {} return is compulsory but in case of () no need to return */
+
+
+// let addTwo = (g1,g4) =>  g1 + g4 //or
+// let addTwo = (g1,g4) => (g1 + g4)
+let detail = () => ({brand: "hp"})
+
+console.log(detail())
+
+//-------------------------Immediately Invoked Function Expressions (IFFE)-----------------------------------
+/* for decare that type of function use two ()() 1 ()  take function definition
+and 2 () for function call. global scope ke pollution se kaibar
+problem hoti hai to use global scope under jo declaration hai, use pollution ko 
+hatane ke liye use IIFE*/ 
+
+
+function detail2(){
+    console.log(`hp laptop`);
+}
+detail2(); 
+/* hitesh ne thai jay che mare nath thatu */
+
