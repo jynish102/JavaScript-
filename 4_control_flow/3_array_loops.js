86d5ef34b0c02e16595b7f59614e321b1a500a22
jyni22 <jaykal67kal@gmail.com>
2025-09-29 20:38:57 +0530

    Update 3_array_loops.js

--- 4_control_flow/3_array_loops.js
+++ 4_control_flow/3_array_loops.js
@@ -16,7 +16,6 @@ for(let greet of greeting)
     // console.log(`each character in ${greet}`)
 
 }
-
 //----------------------MAP--------------get only unique data
 let shortcut ={
     js : "javascript",
@@ -26,7 +25,6 @@ let shortcut ={
  for (let key in shortcut){
     // console.log(`${key} is shortcut for ${shortcut[key]}`)
 }
-
 
 shortcut =["js", "rb", "cpp",'php']
 for(let word in shortcut){
@@ -44,7 +42,6 @@ for(let [key, value] of map){
     // console.log(key, ':-', value);
 }
 
-
 let myObj = {
     game1 : 'NFS',
     game2 : 'Spiderman'
@@ -53,7 +50,6 @@ let myObj = {
 // for(const [key ,value] of myObj){
 //     console.log(key , ':-', value);
 // }
-
 
 let lan ={
     cpp : 'C++',
@@ -65,9 +61,6 @@ let lan ={
 //     console.log(`${key} shortcut is for ${lan[key]}`)
 // }
 
-
-
-
 shortcut =["js", "rb", "cpp",'php']
 // shortcut.forEach ( function (item) {
 //     console.log(item)
@@ -78,19 +71,14 @@ shortcut.forEach( (item) => {
     // console.log(item)
 });
 
-
 function printMe(item){
     // console.log(item)
-
 }
-
 // shortcut.forEach(printMe)
 
 shortcut.forEach( (item, index,arr) => {
     // console.log(item, index, arr)
 })
-
-
 
 let myCodding = [
     {langName : "javascript",
@@ -106,20 +94,15 @@ let myCodding = [
     }
 ]
 
-
 myCodding.forEach( (item) => {
     // console.log(item);
 })
-
-
 let values = shortcut.forEach( (item) => {
     // console.log(item); no error
 }) 
-
 // console.log(values)undefined
 
 let num = [7,1,5,6,8,2,77,89,54]
-
 let filterarray = num.filter( (i) =>i>1) // i is any variable type
 console.log(filterarray)  
 /* if i write arrow function like () => {} show here i am using {} ,i need to
@@ -134,8 +117,6 @@ num.forEach( (i) => {
     }
 })
 // console.log(newArray)
-
-
 
 //---------------------------Filter--------------------------------
 const books = [
@@ -176,7 +157,6 @@ const books = [
   }
 ];
 
-
 // let userBooks = books.filter( (bk) => {
 //     return bk.genre === 'Software Engineering'
 // })
@@ -189,10 +169,7 @@ userBooks = books.filter( (bk) =>
      bk.year >2000
 )
 // console.log(userBooks)
-
-
 let array = [1,2,2,7,8,6,4,45,12]
-
 // let addArray = array.map( (i) => i+10)
 let  num1 =array.map( (i) => i*2)
                 .map( (i) => i +1)
@@ -212,6 +189,3 @@ const myTotal = n1.reduce(
 console.log(myTotal)
 
 /* see video 30 at 31:00 this is use to sum all numbers */
-
-
-
